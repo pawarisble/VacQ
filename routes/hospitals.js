3166b71cc0576179b9863856b22876565dca6c55
pawarisble <blebikebe@gmail.com>
2023-02-13 18:17:11 +0700

    add protect and cookie

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const {getHospital,getHospitals,deleteHospital,createHospital,updateHospital} = require('../controllers/hospitals');
 const router = express.Router();
+const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(getHospitals).post(createHospital);
-router.route('/:id').get(getHospital).put(updateHospital).delete(deleteHospital);
+
+router.route('/').get(getHospitals).post(protect, authorize("admin"),createHospital);
+router.route('/:id').get(getHospital).put(protect,authorize("admin") ,updateHospital).delete(protect,authorize("admin"), deleteHospital);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -10,8 +11,10 @@ const app = express();
 app.use(express.json());
 
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 app.use('/api/v1/hospitals', hospitals)
-
+app.use('/api/v1/auth', auth);
+app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log("Server running in ", process.env.NODE_ENV, " mode on port ", PORT));
 
